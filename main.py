f1f7a01ce9fd44a9d190f27c856c1bf00ef064ba
CoreByte <21132799+gCoreByte@users.noreply.github.com>
2022-09-02 19:44:07 +0300

    wip commit

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import asyncio
+import yaml
+from integrations.discord_hook import DiscordWebhook
+
+def init_discord_webhooks(config_file_path):
+    discord_webhooks = []
+    with open(config_file_path, mode="r", encoding='utf-8') as file:
+        config = yaml.safe_load(file)
+        for url in config.webhook_urls:
+            discord_webhooks.append(DiscordWebhook(url))
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
