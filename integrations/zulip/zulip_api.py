fdccfcf1ea9b17c826261971e4fa424f382e9f6f
CoreByte <21132799+gCoreByte@users.noreply.github.com>
2022-09-04 01:44:17 +0300

    Move zulip files to same package

---
+++ integrations/zulip/zulip_api.py
@@ -0,0 +1,7 @@
+# TODO: map these out
+EVENT_TYPES = {"message"}
+
+# All valid endpoints that we currently use
+REGISTER_EVENT_QUEUE = "https://corebyte.zulipchat.com/api/v1/register"
+GET_EVENTS_FROM_QUEUE = "https://corebyte.zulipchat.com/api/v1/events"
+
