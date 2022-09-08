b64038aaa4831b67e90b4fea7838f2608859d930
gCoreByte <21132799+gCoreByte@users.noreply.github.com>
2022-09-08 16:03:25 +0300

    adds basic api

---
+++ integrations/campuswire/campuswire_api.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+# TODO: map these out
+EVENT_TYPES_RECEIVABLE = {
+  "presence-changed",
+  "user-points-changed",
+  "hello",
+  "message",
+  "typing"
+  }
+
+
+class EventType(Enum):
+    PresenceChanged = 'presence-changed'
+    UserPointsChanged = 'user-points-changed'
+    Hello = 'hello'
+    World = 'world'
+    Message = 'message' # TODO: how to differentiate between sent and received?
+    Typing = 'typing'
+
+
+
+# All valid endpoints that we currently use
+LOGIN_ENDPOINT = "https://api.campuswire.com/v1/auth/login"
